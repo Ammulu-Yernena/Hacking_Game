5733337597cf38b1b08a987fe374d05d0eefb9b2
Ammulu-Yernena <119793970+Ammulu-Yernena@users.noreply.github.com>
2023-11-19 16:50:46 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let name = prompt(" Enter your name ");
+let arr = [
+  "Initializing hack program . . .",
+  `Hacking ${name}'s Galaxy username . . .`,
+  `Username found ${name} . . .`,
+  "Connecting to instagram . . .",
+  "Will complete instagram hacking after 10 sec",
+  "Successfully hacked",
+  `Thank you ${name}`,
+];
+let fun = async (seconds) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(true);
+    }, seconds * 1000);
+  });
+};
+let fun2 = async (src) => {
+  await fun(2);
+  text = document.getElementById("body");
+  text.innerHTML = text.innerHTML + src + "<br>";
+};
+(async () => {
+  for (let i = 0; i < arr.length; i++) {
+    await fun2(arr[i]);
+  }
+})();
